dafebea838679690a0001d2b9db30af35928e436
kyungminJ <31791800+kyungminJ@users.noreply.github.com>
2019-06-03 21:15:19 +0900

    join 폼 체크

--- public/controller/joinController.js
+++ public/controller/joinController.js
@@ -16,7 +16,7 @@ $(document).ready(function() {
         }
     });
 
-    $("#loc_num").change(()=>{
+    $("#loc_num").change(() => {
         let selectNum = $("#loc_num option:selected").val();
         $.ajax({
             url: "http://222.117.225.28:8071/api/user/getLocSchools",
@@ -36,7 +36,7 @@ $(document).ready(function() {
         });
     });
 
-    $("#dupleCheck").click(()=>{
+    $("#dupleCheck").click(() => {
         let checkID = $("#user_id").val();
         if(checkID === "") {
             alert("아이디를 입력해주세요.");
@@ -55,6 +55,7 @@ $(document).ready(function() {
                     $("#checked").val("true");
                     alert("사용가능한 아이디입니다.");
                 } else { // 해당 아이디의 count 가 0 이 아니라면 즉, 중복된다면
+                    $("#checked").val("false");
                     alert("중복되는 아이디입니다. 다시 입력해주세요");
                     $("#user_id").focus();
                 }
@@ -63,5 +64,38 @@ $(document).ready(function() {
                 console.log(error);
             }
         });
+    });
+
+    $("#joinSubmitBtn").click(() => {
+        if($("#user_id").val() === "") {
+            alert("아이디를 입력해주세요.");
+            $("#user_id").focus();
+            return false;
+        }
+
+        if($("#user_pw").val() === "") {
+            alert("비밀번호를 입력해주세요.");
+            $("#user_pw").focus();
+            return false;
+        }
+
+        if($("#user_name").val() === "") {
+            alert("이름을 입력해주세요.");
+            $("#user_name").focus();
+            return false;
+        }
+
+        if($("#loc_num").val() === "" || $("#sn_code").val() === "") {
+            alert("학교를 선택해주세요.");
+            return false;
+        }
+
+        if($("#user_hak").val() === "") {
+            alert("학번을 입력해주세요. (ex 19)");
+            $("#user_hak").focus();
+            return false;
+        }
+
+        //$("#joinForm").submit();
     });
 });
