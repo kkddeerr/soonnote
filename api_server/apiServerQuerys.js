c17c8935f2842a3bc0c890a99e2f5423b0ded8ea
kyungminJ <31791800+kyungminJ@users.noreply.github.com>
2019-06-06 12:20:47 +0900

    비밀번호 암호화

--- api_server/apiServerQuerys.js
+++ api_server/apiServerQuerys.js
@@ -12,7 +12,7 @@ const querys = {
     getAllLocations : "select * from sn_location;",
     getLocSchools : "select SC_NUM, SC_NAME from sn_schools where LOC_NUM = ?;",
     dupleCheck : "select count(*) as count from sn_users where USER_ID = ?;",
-    joinUser : "insert into sn_users (USER_ID,USER_PW,USER_NAME,USER_HAK,SC_NUM,POS_NUM) values (?,?,?,?,?,?);"
+    joinUser : "insert into sn_users (USER_ID,USER_PW,USER_NAME,USER_HAK,SC_NUM,POS_NUM) values (?,password(?),?,?,?,?);"
 };
 
 module.exports = querys;
