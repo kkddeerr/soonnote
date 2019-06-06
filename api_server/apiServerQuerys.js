ddfc9e5359480d75d60d0e82c0ad4d837f3420c4
KangDongHyuk <kkddeerr@naver.com>
2019-06-06 19:26:20 +0900

    apiServerQuery Commit

--- api_server/apiServerQuerys.js
+++ api_server/apiServerQuerys.js
@@ -4,7 +4,7 @@ const querys = {
     // insertMailInfo : "INSERT INTO JA_MAIL (email, farm_num, reg_dt, update_dt) VALUES (?, ?, ?, ?);",
 
     insertCalendarInfo : "insert into sn_cal (CA_ID,CA_EVDATE,CA_STEXT,CA_ERDATE,CA_AEDATE,CA_USEYN) values (?,?,?,?,?,?);",
-    getCalendarData : "select * from sn_cal where CA_EVDATE like ?;",
+    getCalendarData : "select A.*,(select max(CA_NUM) from soonnote.sn_cal) as CA_MAXID from soonnote.sn_cal A where CA_EVDATE like ?",
 
     /*******************************
      * 사용자 관련 쿼리
