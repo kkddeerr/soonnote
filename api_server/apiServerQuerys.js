a6220ce152e3067998d62bd3748b19a3958149af
kyungminJ <31791800+kyungminJ@users.noreply.github.com>
2019-06-06 17:40:28 +0900

    쿼리 파일 수정

--- api_server/apiServerQuerys.js
+++ api_server/apiServerQuerys.js
@@ -3,21 +3,21 @@ const querys = {
     // getUserMotorStatusList : "SELECT type, warn_upper_limit, warn_lower_limit, crit_upper_limit, crit_lower_limit FROM JA_STATUS WHERE farm_num = ? AND `type` IN ('motor_temp' , 'current' , 'vib' , 'sound');",
     // insertMailInfo : "INSERT INTO JA_MAIL (email, farm_num, reg_dt, update_dt) VALUES (?, ?, ?, ?);",
 
-    insertCalendarInfo : "insert into sn_cal (CA_ID,CA_EVDATE,CA_STEXT,CA_ERDATE,CA_AEDATE,CA_USEYN) values (?,?,?,?,?,?);",
-    getCalendarData : "select * from sn_cal where CA_EVDATE like ?;",
+    insertCalendarInfo : "insert into sn_cal (CA_ID,CA_EVDATE,CA_STEXT,CA_ERDATE,CA_AEDATE,CA_USEYN) values (?,?,?,?,?,?);"
+    ,getCalendarData : "select * from sn_cal where CA_EVDATE like ?;"
 
     /*******************************
      * 사용자 관련 쿼리
     *******************************/
-    getAllLocations : "select * from sn_location;",
-    getLocSchools : "select SC_NUM, SC_NAME from sn_schools where LOC_NUM = ?;",
-    dupleCheck : "select count(*) as count from sn_users where USER_ID = ?;",
-<<<<<<< HEAD
+    ,getAllLocations : "select * from sn_location;"
+    ,getLocSchools : "select SC_NUM, SC_NAME from sn_schools where LOC_NUM = ?;"
+    ,dupleCheck : "select count(*) as count from sn_users where USER_ID = ?;"
+    ,joinUser : "insert into sn_users (USER_ID,USER_PW,USER_NAME,USER_HAK,SC_NUM,POS_NUM) values (?,password(?),?,?,?,?);"
 
     /*******************************
      * CCM 관련 쿼리
     *******************************/    
-    getCCMList : `SELECT CM_ID
+    ,getCCMList : `SELECT CM_ID
                        , CM_TITLE
                        , CM_SONG
                        , IFNULL(CM_LOOKUP_COUNT,0) AS CM_LOOKUP_COUNT
@@ -28,9 +28,6 @@ const querys = {
                   WHERE 1=1
                     AND DEL_YN != 'Y';` 
 
-=======
-    joinUser : "insert into sn_users (USER_ID,USER_PW,USER_NAME,USER_HAK,SC_NUM,POS_NUM) values (?,password(?),?,?,?,?);"
->>>>>>> e98c926d1a2877e3195f4c30a4612b22b311a958
 };
 
 module.exports = querys;
