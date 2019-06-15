1d75e3616600cf62f3381a42cf52a97a916f36d6
kbiid <kbiid@naver.com>
2019-06-15 21:39:21 +0900

    수정

--- api_server/routes/api/index.js
+++ api_server/routes/api/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const calendar = require('./calendar/calendarIndex');
 const user = require('./user/userIndex');
 const ccm = require('./ccm/ccmIndex');
+const board = require('./board/boardIndex');
 
 console.log("API route init");
 // 캘린더 / 
